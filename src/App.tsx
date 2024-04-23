eb329d814a8cef687d2d5bea684f49fbf5dd438f
Samuel Gonzalez <Nomekop65@yahoo.com>
2024-04-22 20:49:47 -0400

    Game board updated to handle state and accept clicks

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export function App() {
+  const [game, setGame] = useState({
+    board: [
+      [' ', ' ', ' '],
+      [' ', ' ', ' '],
+      [' ', ' ', ' '],
+    ],
+    id: null,
+    winner: null,
+  })
+
+  function handleClickCell(row: number, column: number) {
+    console.log(`You clicked on row ${row} and column ${column}`)
+  }
+
   return (
     <div>
       <h1>
         Tic Tac Toe - <button>New</button>
       </h1>
       <ul>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
+        <li onClick={() => handleClickCell(0, 0)}>{game.board[0][0]}</li>
+        <li onClick={() => handleClickCell(0, 1)}>{game.board[0][1]}</li>
+        <li onClick={() => handleClickCell(0, 2)}>{game.board[0][2]}</li>
+        <li onClick={() => handleClickCell(1, 0)}>{game.board[1][0]}</li>
+        <li onClick={() => handleClickCell(1, 1)}>{game.board[1][1]}</li>
+        <li onClick={() => handleClickCell(1, 2)}>{game.board[1][2]}</li>
+        <li onClick={() => handleClickCell(2, 0)}>{game.board[2][0]}</li>
+        <li onClick={() => handleClickCell(2, 1)}>{game.board[2][1]}</li>
+        <li onClick={() => handleClickCell(2, 2)}>{game.board[2][2]}</li>
       </ul>
     </div>
   )
