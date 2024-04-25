f197ff5a6e259667772450d9c19d3622c244e9c0
Samuel Gonzalez <Nomekop65@yahoo.com>
2024-04-25 19:34:57 -0400

    App now works with API to create new game and send a move and receive the new board state

--- src/App.tsx
+++ src/App.tsx
@@ -11,14 +11,44 @@ export function App() {
     winner: null,
   })
 
-  function handleClickCell(row: number, column: number) {
-    console.log(`You clicked on row ${row} and column ${column}`)
+  async function handleNewGame() {
+    const response = await fetch(
+      'https://sdg-tic-tac-toe-api.herokuapp.com/game',
+      {
+        method: 'POST',
+        headers: { 'content-type': 'application/json' },
+      }
+    )
+
+    if (response.ok) {
+      const newGame = await response.json()
+
+      setGame(newGame)
+    }
+  }
+  async function handleClickCell(row: number, column: number) {
+    // Generate the URL we need
+    const url = `https://sdg-tic-tac-toe-api.herokuapp.com/game/${game.id}`
+    // Make an object to send as JSON
+    const body = { row: row, column: column }
+    // Make a POST request to make a move
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify(body),
+    })
+    if (response.ok) {
+      // Get the response as JSON
+      const newGame = await response.json()
+      // Make that the new state!
+      setGame(newGame)
+    }
   }
 
   return (
     <div>
       <h1>
-        Tic Tac Toe - <button>New</button>
+        Tic Tac Toe - <button onClick={handleNewGame}>New</button>
       </h1>
       <ul>
         <li onClick={() => handleClickCell(0, 0)}>{game.board[0][0]}</li>
